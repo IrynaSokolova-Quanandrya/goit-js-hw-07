574d3f9407b225b917118c1310fd7c6e5d54938e
Iryna <irasokolova25@ukr.net>
2021-07-19 12:49:59 +0300

    Добавляет 2 задание

---
+++ src/js/task-01.js
@@ -0,0 +1,22 @@
+/*const totalEl = document.querySelector('#categories');
+console.log(totalEl);
+
+const quantityEl = document.querySelectorAll('.item');
+console.log(`В списке ${quantityEl.length} категории`);
+
+
+
+
+
+
+const titleEl = document.querySelectorAll('h2');
+console.log(titleEl. textContent)
+
+console.log('Категория', );
+
+
+const quantityCategoryEl = document.querySelectorAll('ul>li')
+console.log(quantityCategoryEl);
+console.log('Количество элементов:', ); */
+
+
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const listIngredientsEl = document.querySelector('#ingredients');
+console.log(listIngredientsEl);
+
+const makeIngredientsList = ingredients.forEach(
+  ingredient => {
+const listEl = document.createElement('li');
+listEl.innerText = ingredient;
+
+listIngredientsEl.append(listEl);
+  });
+
+  console.log();
+
+
+
