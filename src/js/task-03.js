61734271dce8a5d092347fbc72396d964b3eecf2
Iryna <irasokolova25@ukr.net>
2021-07-19 14:23:25 +0300

    Добавляет 3 пункт

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,40 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+/*Напиши скрипт для создания галлереи изображений по массиву данных.
+
+В HTML есть список ul#gallery.
+
+<ul id="gallery"></ul>
+Используй массив объектов images для создания тегов img вложенных в li. 
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через css-классы. */
+
+const listGalleryEl = document.querySelector('#gallery');
+
+
+const image = images[0];
+const makeImagesList = images.map(
+    image => {
+const listEl = document.createElement('li'); 
+    listEl.classList = 'gallery__item';
+
+const imageEl = document.createElement('img');
+    imageEl.src = image.url;
+    imageEl.alt = image.alt;
+    imageEl.width = 320;
+    imageEl
+
+listEl.appendChild(imageEl);
+ 
+
+listGalleryEl.append(listEl);
+
+  });
+console.log(listGalleryEl)
+
+
+//listGalleryEl.insertAdjacentHTML('', '');
