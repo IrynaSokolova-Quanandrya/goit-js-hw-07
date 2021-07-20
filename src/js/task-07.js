87694ef6e41c4e4d9c8d1585263a11d46be0c418
Iryna <irasokolova25@ukr.net>
2021-07-20 23:59:29 +0300

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,8 +1,10 @@
-/*const input = document.querySelector('#font-size-control');
 
-const output = document.querySelector('#text');
-input.addEventListener('input', () => {
-    let volume = input.value;
-    output.style.fontSize = (volume / 50) + "em";
-}
-); */
+const range = document.querySelector('#font-size-control');
+const rangeText = document.querySelector('#text');
+
+
+range.addEventListener('input', () => {
+    rangeText.style.fontSize = range.value + 'px';
+});
+
+/*Done */
